12216bccb37adab62a342f67a8b6ef8d2758d001
Jane Weggenmann <janeweggenmann@gmail.com>
2021-07-23 19:21:11 -0400

    Update main-view and movie-card

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,33 +1,18 @@
 import React from "react";
 import axios from "axios";
 
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
+
+export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: "60dbc6157af35137390c7d74",
-          Title: "Silence of the Lambs",
-          Description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-          Image: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg"
-        },
-        {
-          _id: "60dd29cbefba3a45116c9553",
-          Title: "Home Alone",
-          Description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.",
-          Image: "https://upload.wikimedia.org/wikipedia/en/7/76/Home_alone_poster.jpg"
-        },
-        {
-          _id: "60dd2a71efba3a45116c9559",
-          Title: "Cast Away",
-          Description: "A FedEx executive undergoes a physical and emotional transformation after crash landing on a deserted island.",
-          Image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Cast_away_film_poster.jpg/220px-Cast_away_film_poster.jpg"
-        }
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
   }
 
@@ -44,15 +29,27 @@ import { MovieView } from "../movie-view/movie-view";
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  //when movie is selected, update state from null to the selected movie
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  //when a user logs in, update state from null to that user
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
     const { movies, selectedMovie } = this.state;
 
+    //if there is no user, show the LoginView
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    //if there are no movies, return blank
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
     return (
-      <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+      <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Name}</div>
     );
   }
 }
