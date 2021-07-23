d2246f0528fb80d6573a4e9ba2f4e04e282a685f
Jane Weggenmann <janeweggenmann@gmail.com>
2021-07-23 19:20:37 -0400

    Add prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 export class MovieCard extends React.Component {
   render() {
@@ -9,3 +10,23 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Year: PropTypes.string,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    }),
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import axios from "axios";
 
 export class MovieView extends React.Component {
+
   render() {
     const { movie, onBackClick } = this.props;
     return (
       <div className="movie-view" onClick={() => { onBackClick(null); }}>
         <div className="movie-poster">
-          <img src={movie.Image} />
+          <img src={movie.ImageURL} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <span className="value">{movie.Name}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
