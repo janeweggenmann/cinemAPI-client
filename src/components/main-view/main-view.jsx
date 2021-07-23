f181a5b803cb981af259257dcb75509186518c8f
Jane Weggenmann <janeweggenmann@gmail.com>
2021-07-23 18:00:03 -0400

    Install axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+import React from "react";
+import axios from "axios";
 
-export class MainView extends React.Component {
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
   constructor() {
     super();
@@ -31,6 +31,19 @@ export class MainView extends React.Component {
     };
   }
 
+  componentDidMount() {
+    axios
+      .get("https://weggenmann-cinemapi.herokuapp.com/movies")
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
@@ -39,13 +52,29 @@ export class MainView extends React.Component {
 
   render() {
     const { movies, selectedMovie } = this.state;
-    if (selectedMovie) return <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)}
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={newSelectedMovie => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map(movie => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={newSelectedMovie => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))
+        )}
       </div>
     );
   }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 export class MovieView extends React.Component {
   render() {
