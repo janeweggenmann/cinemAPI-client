684050c9878b0002f9b00d347f7c97d9068b7fbc
Jane Weggenmann <janeweggenmann@gmail.com>
2021-08-01 19:07:11 -0400

    Remove FloatingLabel

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import "./login-view.scss";
 import Form from "react-bootstrap/Form";
 import Button from 'react-bootstrap/Button';
-import FloatingLabel from "react-bootstrap-floating-label";
 
 
 export function LoginView(props) {
