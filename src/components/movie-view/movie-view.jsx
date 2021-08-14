c064c2f954e38570388d8550a24a6b118ee76972
Jane Weggenmann <janeweggenmann@gmail.com>
2021-08-14 14:47:03 -0400

    Update addFavorite function

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import "./movie-view.scss";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
 
 export class MovieView extends React.Component {
+  addFavorite() {
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("user");
+
+    axios
+      .post(
+        `https://weggenmann-cinemapi.herokuapp.com/users/${username}/favorites/${this.props.movie._id}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+      )
+      .then(response => {
+        console.log("Added to Favorites");
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
     const year = new Date(movie.Year);
-    const movieyear = (year.getFullYear());
+    const movieyear = year.getFullYear();
     return (
-      <Row className="movie-view" >
+      <Row className="movie-view">
         <Col md={12}>
           <img className="movie-view_image" src={movie.ImageURL} />
         </Col>
@@ -20,21 +41,47 @@ export class MovieView extends React.Component {
           <div className="movie-view_wrapper">
             <h2 className="movie-view_title">{movie.Name}</h2>
             <div>
-              <p className="movie-view_label">Overview:  <span className="movie-view_text"> {movie.Description}</span></p>
-              <p className="movie-view_label">Year:  <span className="movie-view_text"> {movieyear}</span></p>
+              <p className="movie-view_label">
+                Overview:{" "}
+                <span className="movie-view_text"> {movie.Description}</span>
+              </p>
+              <p className="movie-view_label">
+                Year: <span className="movie-view_text"> {movieyear}</span>
+              </p>
               <span className="movie-view_label">Directed By: </span>
               <Link to={`/directors/${movie.Director.Name}`}>
                 <span className="movie-view_text">{movie.Director.Name}</span>
               </Link>
               <p></p>
-              <span className="movie-view_label">Genre:  </span>
+              <span className="movie-view_label">Genre: </span>
               <Link to={`/genres/${movie.Genre.Name}`}>
                 <span className="movie-view_text"> {movie.Genre.Name}</span>
               </Link>
               <p></p>
             </div>
-            <div className="d-flex align-items-start">
-              <button className="movie-view_button" onClick={() => { onBackClick(null); }}>Back</button>
+            <div className="movie-view_buttons">
+              <button
+                className="movie-view_button"
+                onClick={() => {
+                  onBackClick(null);
+                }}
+              >
+                Back
+              </button>
+              <Popup
+                trigger={
+                  <button
+                    className="movie-view_favorite-button"
+                    value={movie.id}
+                    onClick={e => this.addFavorite(e, movie)}
+                  >
+                    Favorite
+                  </button>
+                }
+                position="right center"
+              >
+                <div>Added to favorites</div>
+              </Popup>
             </div>
           </div>
         </Col>
