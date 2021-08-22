ab0cc59c4c2979dc11f93118e02615ddac2f98c2
Jane Weggenmann <janeweggenmann@gmail.com>
2021-08-21 23:15:21 -0400

    Create actions and reducers

---
+++ src/reducers/reducers.js
@@ -0,0 +1,30 @@
+import { combineReducers } from "redux";
+import { SET_FILTER, SET_MOVIES } from "../actions/actions";
+
+// switch-case function - if an action is called that the reducer is not responsible for, it will just return what is was given
+// the reducer only cares about the action it's responsible for
+function visibilityFilter(state = "", action) {
+  switch (action.type) {
+    case SET_FILTER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function movies(state = [], action) {
+  switch (action.type) {
+    case SET_MOVIES:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+// this is a combined reducer, made of the 2 reducers above
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies
+});
+
+export default moviesApp;
