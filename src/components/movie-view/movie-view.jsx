a49b29d62a0cae32cfdf9f997b1b70e94c7be2cb
Jane Weggenmann <janeweggenmann@gmail.com>
2021-08-25 19:30:54 -0400

    Add reload when add favorite movie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -56,6 +56,7 @@ export class MovieView extends React.Component {
       )
       .then(response => {
         alert("Added to favorites!");
+        window.location.reload();
       })
       .catch(function (error) {
         console.log(error);
